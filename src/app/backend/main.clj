eefa6053660a38677084b2743abaed8c33fc48d3
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2026-04-01 16:06:32 -0700

    Use PORT env var

--- src/app/backend/main.clj
+++ src/app/backend/main.clj
@@ -2,5 +2,12 @@
   (:gen-class)
   (:require [app.backend.server :as server]))
 
+(defn env-port []
+  (when-some [port-value (System/getenv "PORT")]
+    (or (parse-long port-value)
+        (throw (ex-info "Invalid PORT value"
+                        {:port-value port-value})))))
+
 (defn -main [& _args]
-  (server/start-server {:port 8080 :join? true}))
+  (server/start-server {:port (or (env-port) 8080)
+                        :join? true}))
